13dd4fb96c0f1a1e42808616bb7eed6312f494f1
skramer <skramer5@luc.edu>
2025-04-09 17:40:07 -0500

    Added basic test loading for collections to the main page

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function mainApp() {
+	const noCollections = document.getElementById('no-collections');
+
 	// create a popup window of newCollection.html when the user clicks on the new collection button
 	let newCollectionButton = document.getElementById('new-collection-button');
 	newCollectionButton.addEventListener('click', () => {
@@ -10,6 +12,30 @@ function mainApp() {
 	addItemButton.addEventListener('click', () => {
 		window.open('addItem.html', 'Add a new item', 'width=400, height=600, popup=true');
 	});
+
+	fetch('testCollections.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json();
+		})
+		.then(jsonData => {
+			const collectionsDisplay = document.getElementById('collections');
+			const collections = jsonData['collections'].reduce((accumulator, currentVal) => {
+                const container = document.createElement('div');
+				container.classList.add('displayCollection');
+                const collectionText = document.createTextNode(currentVal.name);
+                container.appendChild(collectionText);
+                accumulator.appendChild(container);
+                return accumulator;
+            }, document.createElement('div'));
+            collectionsDisplay.appendChild(collections);
+			noCollections.style.display = 'none';
+		})
+		.catch(error => {
+			console.log("error loading collections" + error);
+		});
 }
 
 mainApp();
