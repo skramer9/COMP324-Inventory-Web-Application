eca4810f1346b5f123c931215d5251c924b7f281
skramer <skramer5@luc.edu>
2025-04-09 21:01:47 -0500

    Made the collections on main.html display with links as to collection.html

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 function mainApp() {
-	const noCollections = document.getElementById('no-collections');
 
 	// create a popup window of newCollection.html when the user clicks on the new collection button
 	let newCollectionButton = document.getElementById('new-collection-button');
@@ -10,32 +9,42 @@ function mainApp() {
 	// create a popup window of addItem.html when the user clicks on the add item button
 	let addItemButton = document.getElementById('add-item-button');
 	addItemButton.addEventListener('click', () => {
-		window.open('addItem.html', 'Add a new item', 'width=400, height=600, popup=true');
+		window.open('newItem.html', 'Add a new item', 'width=400, height=600, popup=true');
 	});
 
+	loadCollections();
+}
+
+function loadCollections() {
+	const noCollections = document.getElementById('no-collections');
+
 	fetch('testCollections.json')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            return response.json();
-		})
-		.then(jsonData => {
-			const collectionsDisplay = document.getElementById('collections');
-			const collections = jsonData['collections'].reduce((accumulator, currentVal) => {
-                const container = document.createElement('div');
-				container.classList.add('displayCollection');
-                const collectionText = document.createTextNode(currentVal.name);
-                container.appendChild(collectionText);
-                accumulator.appendChild(container);
-                return accumulator;
-            }, document.createElement('div'));
-            collectionsDisplay.appendChild(collections);
-			noCollections.style.display = 'none';
-		})
-		.catch(error => {
-			console.log("error loading collections" + error);
-		});
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+	})
+	.then(jsonData => {
+		const collectionsDisplay = document.getElementById('collections');
+		const collections = jsonData['collections'].reduce((accumulator, currentVal) => {
+            const container = document.createElement('div');
+			const link = document.createElement('a');
+			link.href = 'collection.html';
+			link.classList.add('collection-link');
+			container.classList.add('display-collection');
+            const collectionText = document.createTextNode(currentVal.name);
+			link.appendChild(collectionText);
+            container.appendChild(link);
+            accumulator.appendChild(container);
+            return accumulator;
+        }, document.createElement('div'));
+        collectionsDisplay.appendChild(collections);
+		noCollections.style.display = 'none';
+	})
+	.catch(error => {
+		console.log("error loading collections: " + error);
+	});
 }
 
 mainApp();
